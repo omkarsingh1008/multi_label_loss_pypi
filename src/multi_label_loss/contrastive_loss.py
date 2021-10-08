5078aa92bb279baec5193b5555f775ff13083a2b
omkarsingh1008 <omkarsinghrajpurohit363@gmail.com>
2021-10-08 21:57:13 +0530

    contrastive_loss

---
+++ src/multi_label_loss/contrastive_loss.py
@@ -0,0 +1,11 @@
+import tensorflow.keras.backend as K
+
+def contrastive_loss_with_margin(margin):
+    def contrastive_loss(y_true, y_pred):
+        '''Contrastive loss from Hadsell-et-al.'06
+        http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+        '''
+        square_pred = K.square(y_pred)
+        margin_square = K.square(K.maximum(margin - y_pred, 0))
+        return K.mean(y_true * square_pred + (1 - y_true) * margin_square)
+    return contrastive_loss
